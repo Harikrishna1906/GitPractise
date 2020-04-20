54af26360972692f3c534cf9811ef0e6e7505e48
Harikrishna1906 <hari_r@live.com>
2020-04-20 18:18:00 +0530

    Committed from the Y person

--- src/test/java/Cigniti/AppiumTest.java
+++ src/test/java/Cigniti/AppiumTest.java
@@ -7,6 +7,9 @@ public class AppiumTest {
 	@Test
 	public void nativeAppsAndroid() {
 		System.out.println("nativeAppsAndroid");
+		System.out.println("nativeAppsAndroid_1");
+		System.out.println("nativeAppsAndroid_2");
+		System.out.println("nativeAppsAndroid_3");
 	}
 	@Test
 	public void HybridAppsIOS() {
