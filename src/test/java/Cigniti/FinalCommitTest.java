686bb1ca9b89f5b42c36e2233f7491bb5c5397ec
Harikrishna1906 <hari_r@live.com>
2020-04-21 13:21:37 +0530

    The final Commit by Architect

---
+++ src/test/java/Cigniti/FinalCommitTest.java
@@ -0,0 +1,55 @@
+package Cigniti;
+
+import org.testng.annotations.Test;
+
+public class FinalCommitTest {
+	
+	@Test
+	public void BrowserTest() {
+		System.out.println("BrowserTest");
+		System.out.println("BrowserTest");
+
+		System.out.println("BrowserTest");
+
+		System.out.println("BrowserTest");
+
+		System.out.println("BrowserTest");
+
+	}
+	
+	@Test
+	public void BrowserTest1() {
+		System.out.println("BrowserTest");
+		System.out.println("BrowserTest");
+
+		System.out.println("BrowserTest");
+
+		System.out.println("BrowserTest");
+
+		System.out.println("BrowserTest");
+
+	}
+	@Test
+	public void UITest() {
+		System.out.println("UITest");
+	}
+	
+	@Test
+	public void UITest1() {
+		System.out.println("UITest");
+	}
+	
+
+	@Test
+	public void UITest2() {
+		System.out.println("UITest");
+	}
+	
+	
+
+	@Test
+	public void UITest3() {
+		System.out.println("UITest");
+	}
+
+}
